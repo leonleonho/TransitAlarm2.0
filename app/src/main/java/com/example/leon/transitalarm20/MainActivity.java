91d64dd1645cdf33ea03d2f6cdec7a8d0f404867
Jens-Lenovo <lambmaster@hotmail.com>
2015-01-18 18:22:40 -0800

    FINALLY figured out how to change the frickin' actionbar and bottom navbar colors...

--- app/src/main/java/com/example/leon/transitalarm20/MainActivity.java
+++ app/src/main/java/com/example/leon/transitalarm20/MainActivity.java
@@ -21,11 +21,12 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
