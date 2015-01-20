7a2d86e44a7bf3629b2cf9a70f9db5a5771b2a05
Jens-Lenovo <lambmaster@hotmail.com>
2015-01-19 20:05:04 -0800

    COMMITTED EVERYTHING THIS TIME Adjusted the Navbar. Added link to Map button on landing page. Removed erroneous button on landing page.

--- app/src/main/java/com/example/leon/transitalarm20/MainActivity.java
+++ app/src/main/java/com/example/leon/transitalarm20/MainActivity.java
@@ -33,10 +33,11 @@ public class MainActivity extends Activity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
+        /*
         if (id == R.id.action_settings) {
             return true;
         }
-
+        */
         return super.onOptionsItemSelected(item);
     }
     public void gotoMap(View view) {
