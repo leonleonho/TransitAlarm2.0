dd0551cc5baf5af5ca505e938c3c012003eb314f
Jens-Lenovo <lambmaster@hotmail.com>
2015-01-31 23:44:42 -0800

    Added simple JSON functionality -type in a bus number, press a button, and the bus stop name appears.

--- app/src/main/java/com/example/leon/transitalarm20/MainActivity.java
+++ app/src/main/java/com/example/leon/transitalarm20/MainActivity.java
@@ -1,23 +1,70 @@
 package com.example.leon.transitalarm20;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 
 
 public class MainActivity extends Activity {
 
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView)findViewById(R.id.tv);
+    }
 
+    public void getJson(View view) {
+        EditText et = (EditText)findViewById(R.id.et);
+        String busNo = et.getText().toString();
+        tv.setText("Loading...");
+        new GetJSONTask().execute(
+                "http://api.translink.ca/rttiapi/v1/stops/" + busNo + "?apikey=uqHksMgJHyOOpCRjXNKM");
+    }
+
+    private class GetJSONTask extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            HttpGet httpGet = new HttpGet(params[0]);
+            httpGet.addHeader("Accept", "application/json");
+            httpGet.addHeader("ContentType", "application/json");
+            DefaultHttpClient client = new DefaultHttpClient();
+            String result = null;
+            try {
+                HttpResponse response = client.execute(httpGet);
+                HttpEntity entity = response.getEntity();
+                result = EntityUtils.toString(entity);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            try {
+                JSONObject json = new JSONObject(s);
+                tv.setText(json.getString("Name"));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 
@@ -34,7 +81,7 @@ public class MainActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        //int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         /*
